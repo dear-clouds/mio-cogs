240f45150bbcd4e098bc97ee4531fc371cf7293e
Mio <mio.bluewatermelon@gmail.com>
2023-06-20 01:32:31 +0700

    [RewardRole] Store user message counts

--- rewardrole/rewardrole.py
+++ rewardrole/rewardrole.py
@@ -1,15 +1,16 @@
 import discord
 import re
 from redbot.core import commands, Config
-from datetime import timedelta
+from datetime import timedelta, datetime
 import asyncio
 
 class RewardRole(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1995987654321)
+        self.config = Config.get_conf(self, identifier=1995987654321, force_registration=True)
         default_guild = {
-            "roles": {}
+            "roles": {},
+            "user_activity": {}
         }
         self.config.register_guild(**default_guild)
         self.bg_task = self.bot.loop.create_task(self.update_roles())
@@ -19,6 +20,7 @@ class RewardRole(commands.Cog):
         while not self.bot.is_closed():
             for guild in self.bot.guilds:
                 roles = await self.config.guild(guild).roles()
+                user_activity = await self.config.guild(guild).user_activity()
                 for role_id, role_data in roles.items():
                     role = guild.get_role(int(role_id))
                     reward_role = guild.get_role(role_data["reward_role"])
@@ -27,19 +29,10 @@ class RewardRole(commands.Cog):
                         if role in member.roles and not any(excluded_role in member.roles for excluded_role in excluded_roles):
                             min_messages = role_data["min_messages"]
                             timeframe = timedelta(days=role_data["timeframe_days"])
-                            after = member.joined_at - timeframe
-                            message_count = 0
+                            message_count = user_activity.get(str(member.id), {}).get('message_count', 0)
+                            last_message = user_activity.get(str(member.id), {}).get('last_message', member.joined_at)
 
-                            for channel in guild.text_channels:
-                                if channel.id in role_data["ignored_channels"]:
-                                    continue
-
-                                messages = await channel.history(limit=100, after=after).flatten()
-                                user_messages = [msg for msg in messages if msg.author == member]
-
-                                message_count += len(user_messages)
-
-                            if message_count >= min_messages:
+                            if message_count >= min_messages and last_message >= datetime.now() - timeframe:
                                 if reward_role not in member.roles:
                                     await member.add_roles(reward_role)
                             else:
@@ -89,15 +82,23 @@ class RewardRole(commands.Cog):
             await ctx.send("No role conditions have been configured.")
             return
 
-        conditions = []
+        embed = discord.Embed(color=discord.Color.blue())
         for role_id, role_data in roles_data.items():
             role = ctx.guild.get_role(int(role_id))
             reward_role = ctx.guild.get_role(role_data["reward_role"])
             excluded_roles = [ctx.guild.get_role(excluded_role_id) for excluded_role_id in role_data["excluded_roles"]]
             ignored_channels = [ctx.guild.get_channel(channel_id) for channel_id in role_data["ignored_channels"]]
-            conditions.append(f"Role: {role.name} | Min Messages: {role_data['min_messages']} | Timeframe (days): {role_data['timeframe_days']} | Reward Role: {reward_role.name} | Excluded Roles: {', '.join(excluded_role.name for excluded_role in excluded_roles)} | Ignored Channels: {', '.join(channel.name for channel in ignored_channels)}")
 
-        await ctx.send("\n".join(conditions))
+            field_value = (
+                f"Min Messages: {role_data['min_messages']}\n"
+                f"Timeframe (days): {role_data['timeframe_days']}\n"
+                f"Reward Role: {reward_role.mention}\n"
+                f"Excluded Roles: {', '.join(excluded_role.mention for excluded_role in excluded_roles if excluded_role)}\n"  # Ensure the role exists
+                f"Ignored Channels: {', '.join(channel.mention for channel in ignored_channels if channel)}"  # Ensure the channel exists
+            )
+            embed.add_field(name=f"Role: {role.name}", value=field_value, inline=False)
+
+    await ctx.send(embed=embed)
         
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -108,6 +109,7 @@ class RewardRole(commands.Cog):
             return
 
         roles = await self.config.guild(message.guild).roles()
+        user_activity = await self.config.guild(message.guild).user_activity()
 
         for role_id, role_data in roles.items():
             role = message.guild.get_role(int(role_id))
@@ -120,21 +122,9 @@ class RewardRole(commands.Cog):
                 if re.match(link_only_regex, message.content):
                     return
 
-                # Check messages in the specified timeframe and count them
-                min_messages = role_data["min_messages"]
-                timeframe = timedelta(days=role_data["timeframe_days"])
-                after = message.created_at - timeframe
-
-                messages = []
-                async for msg in message.channel.history(limit=100, after=after):
-                    messages.append(msg)
-                user_messages = [msg for msg in messages if msg.author == message.author]
-
-                if len(user_messages) >= min_messages:
-                    reward_role = message.guild.get_role(role_data["reward_role"])
-                    if reward_role not in message.author.roles:
-                        await message.author.add_roles(reward_role)
-                else:
-                    reward_role = message.guild.get_role(role_data["reward_role"])
-                    if reward_role in message.author.roles:
-                        await message.author.remove_roles(reward_role)
+                # Increase the message count for the user and update the last message timestamp
+                user_activity_data = user_activity.get(str(message.author.id), {"message_count": 0, "last_message": message.created_at})
+                user_activity_data['message_count'] += 1
+                user_activity_data['last_message'] = message.created_at
+                user_activity[str(message.author.id)] = user_activity_data
+                await self.config.guild(message.guild).user_activity.set(user_activity)
